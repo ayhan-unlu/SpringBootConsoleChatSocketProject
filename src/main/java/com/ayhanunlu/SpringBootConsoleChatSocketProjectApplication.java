e3636bd210c07f708b0b76e619a88af28584496b
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-09-09 10:23:40 +0300

    SpringBootConsoleChatSocketProjectApplication.java (CommandLineRunner is removed, Run configuration with compound in intellij is set to open three separate console windows, 1- application,2-server,3-client )

--- src/main/java/com/ayhanunlu/SpringBootConsoleChatSocketProjectApplication.java
+++ src/main/java/com/ayhanunlu/SpringBootConsoleChatSocketProjectApplication.java
@@ -1,6 +1,5 @@
 package com.ayhanunlu;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
